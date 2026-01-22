adde52edfc5df5c3fd422d21d6bf709b0b947cc7
gustavohfesteves-cloud <gustavohfesteves@gmail.com>
2026-01-21 22:40:17 -0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
-import Image from 'next/image';
-import Link from 'next/link';
+import Image from 'src/app';
+import Link from 'src/app';
 
 export default function Login() {
   return (
@@ -48,6 +48,7 @@ export default function Login() {
     </div>
   );
 }
+
 
 
 
